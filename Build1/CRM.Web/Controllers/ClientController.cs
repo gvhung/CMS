f65b07d03bb9e617807c06f2fcd0011b9b8720dd
Venkat5160 <venkat.5160@gmail.com>
2016-06-01 12:50:34 +0530

    Client Controller Added to SVN

---
+++ Build1/CRM.Web/Controllers/ClientController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRM.Model;
+using CRM.Web.Models;
+using CRM.Tickets;
+using CRM.Store;
+using AutoMapper.QueryableExtensions;
+using PagedList;
+
+namespace CRM.Web.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ClientCreateModel m)
+        {
+            Client client = new Client();
+            client.Name = m.Name;
+            client.TicketStartNumber = m.TicketStartNumber;
+
+
+            ClientManager<Client> clientManager = new ClientManager<Client>(new ClientStore<Client>());
+            clientManager.CreateClient(client);
+
+            return RedirectToAction("List");
+
+        }
+
+        [HttpGet]
+        public ActionResult List()
+        {
+           // SearchCriteria criteria = new SearchCriteria();
+           // criteria.Title = Search;
+            ClientManager<Client> clientManager = new ClientManager<Client>
+                (new ClientStore<Client>());
+            //List<TicketListModel> lst = new List<TicketListModel>();
+            //ticketManager.GetTickets().ForEach(t => lst.Add(new TicketListModel() { TicketNo = t.TicketNo == null ? 0:Convert.ToUInt32(t.TicketNo), TicketType = Convert.ToString(t.TicketType), Priority = Convert.ToString(t.Priority),Title=t.Title }));
+            IQueryable<Client> lstClients = clientManager.GetClient();
+            IQueryable<ClientListModel> lstClientListModel = lstClients.ProjectTo<ClientListModel>();
+           // IPagedList<ClientListModel> pagedList = lstClientListModel.ToPagedList(page, 2);
+            return View(lstClientListModel);
+        }
+
+
+
+
+    }
+}
---
+++ Build1/CRM.Web/Models/ClientCreateModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRM.Web.Models
+{
+    public class ClientCreateModel
+    {
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+        public string TicketStartNumber { get; set; }
+    }
+}
