0038f48f2b6b69b42ca234bd0d835b8d92e3c142
manibondada <mkbondada@gmail.com>
2016-06-03 22:31:59 +0530

    IProductStore added

---
+++ Build1/CRM.Tickets/Interfaces/IProductStore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CRM.Model;
+using CRM.Model.Interfaces;
+
+namespace CRM.Tickets.Interfaces
+{
+    public interface IProductStore<TProduct> where TProduct :IProduct
+    {
+        IQueryable<TProduct> GetProducts();
+    }
+}
