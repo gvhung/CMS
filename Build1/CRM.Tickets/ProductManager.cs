7718bd5811d1c0857eb96cfd833c1911553dbfd5
manibondada <mkbondada@gmail.com>
2016-06-03 11:16:35 +0530

    added ProductManager

---
+++ Build1/CRM.Tickets/ProductManager.cs
@@ -0,0 +1,25 @@
+﻿using CRM.Model.Interfaces;
+using CRM.Tickets.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Tickets
+{
+   public class ProductManager<TProduct> where TProduct:IProduct
+    {
+        IProductStore<TProduct> _productStore;
+        public ProductManager(IProductStore<TProduct> productStore)
+        {
+            _productStore = productStore;
+        }
+        public IQueryable<TProduct> GetProducts()
+        {
+
+
+            return _productStore.GetProducts();
+        }
+    }
+}
