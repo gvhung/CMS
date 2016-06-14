f5535c494f2bb61c0fbfe6c8cb3a1cf4b953c88b
manibondada <mkbondada@gmail.com>
2016-06-14 17:09:18 +0530

    create ProductCreateModel in viewmodel

--- Build1/CRM.Web/ViewModel/ProductCreateModel.cs
+++ Build1/CRM.Web/ViewModel/ProductCreateModel.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using CRM.Model;
+
 namespace CRM.Web.ViewModel
 {
     public class ProductCreateModel
     {
+     //   [Required]
+     //   public string Client { get; set; }
         [Required]
+        [Display(Name = "Company")]
+        public string ClientId { get; set; }
         [Display(Name="Product Name")]
         public string Name { get; set; }
 
+        public List<Client> Clients { get; set; }
         public string Description { get; set;}
+        [Display(Name = "versions")]
+        public string Version { get; set; }
     }
 }
